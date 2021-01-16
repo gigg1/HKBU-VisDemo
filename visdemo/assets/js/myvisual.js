3ebf8d4b81bd9b2515e31bb7129357dcd8fe3b64
suyuxin <20403305@life.hkbu.edu.hk>
2021-01-17 01:34:11 +0800

    Separate css and js from html(and in sails)

---
+++ visdemo/assets/js/myvisual.js
@@ -0,0 +1,18 @@
+// <!-- 引入 echarts.js -->
+// <script src="js/echarts.js" type="text/javascript" charset="utf-8"></script>
+// <!-- 引入 map.js -->
+// <script src="js/map.js" type="text/javascript" charset="utf-8"></script>
+// <!-- 引入 linetrend.js -->
+// <script src="js/linetrend.js" type="text/javascript" charset="utf-8"></script>
+// <!-- 引入 twowayhist.js -->
+// <script src="js/twowayhist.js" type="text/javascript" charset="utf-8"></script>
+// <!-- 引入 probplot.js -->
+// <script src="js/probplot.js" type="text/javascript" charset="utf-8"></script>
+// <!-- 引入 horizhist.js -->
+// <script src="js/horizhist.js" type="text/javascript" charset="utf-8"></script>
+document.write('<script src="js/echarts.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/map.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/linetrend.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/twowayhist.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/probplot.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/horizhist.js" type="text/javascript" charset="utf-8"></script>');
