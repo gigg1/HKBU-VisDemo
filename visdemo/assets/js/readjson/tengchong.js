2ddb24403e73071d3853c8dbab230df443dec94b
suyuxin <20403305@life.hkbu.edu.hk>
2021-01-19 17:22:48 +0800

    try show map by load specific Json file

---
+++ visdemo/assets/js/readjson/tengchong.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#tengchong').click(function() {
+        $.getJSON('json/TengChong.json', function(data) {
+            var cell = "<ul>";
+            $.each(data.geometries, function(i, n) {
+                cell += "<li>" + n["type"] + n["coordinates"] + "</li>";
+            });
+            cell += "</ul>";
+            $('#tengchong_result').append(cell);
+        });
+        return false;
+    });
+
+    $("#tengchong_hide").click(function() {
+        $("#tengchong_result").hide();
+    });
+    $("#tengchong_show").click(function() {
+        $("#tengchong_result").show();
+    });
+
+
+});
