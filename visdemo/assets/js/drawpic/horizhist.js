009588c9a92fbc4a9e2833f545741771d674a368
suyuxin <20403305@life.hkbu.edu.hk>
2021-01-26 10:27:58 +0800

    horizhist graph read data from json file

--- visdemo/assets/js/drawpic/horizhist.js
+++ visdemo/assets/js/drawpic/horizhist.js
@@ -1,100 +1,153 @@
 // 采用立即执行函数(function(){})();
 (function() {
-    // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById('youxiajiao'));
 
-    // 指定图表的配置项和数据
-    option = {
-        title: {
-            top: '0%',
-            left: 'center',
-            text: 'Rank of Case Number',
-        },
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow'
-            }
-        },
 
-        dataset: {
-            source: [
-                ['score', 'amount', 'townname'],
-                [89.3, 58212, 'QuShi'],
-                [57.1, 78254, 'HeShun'],
-                [74.4, 41032, 'WuHe'],
-                [50.1, 12755, 'XinHua'],
-                [89.7, 20145, 'JieTou'],
-                [68.1, 79146, 'MaZhan'],
-                [19.6, 91852, 'HeHua'],
-                [10.6, 101852, 'MangBang'],
-                [32.7, 20112, 'TuanTian'],
-                [89.3, 58212, 'QingShui'],
-                [57.1, 78254, 'BeiHai'],
-                [74.4, 41032, 'PuChan'],
-                [50.1, 12755, 'GuDong'],
-                [89.7, 20145, 'ZhongHe'],
-                [68.1, 79146, 'TengYue'],
-                [19.6, 91852, 'DianTan'],
-                [10.6, 101852, 'MingGuang'],
-                [32.7, 20112, 'HouQiao']
-            ]
-        },
-        grid: { containLabel: true },
-        xAxis: { name: 'amount' },
-        yAxis: {
-            type: 'category',
-            // name: 'Town Name',
-            axisLabel: {
-                show: true,
-                textStyle: {
-                    // color: '#fff',
-                    fontSize: '9'
+    // jQuery闭包函数
+    // (function($) {
+    //     $(function() {
+
+    $.get('json/Export_Output.json', function(tengchongdata) {
+
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('youxiajiao'));
+
+        // 指定图表的配置项和数据
+        // var townname = ['townname'];
+        // var childNum = ['amount'];
+        var data_set_from_json = [];
+        data_set_from_json.push(['amount', 'townname']);
+        var agency = [];
+        data_set_from_json.push(agency);
+
+        // console.log(tengchongdata.features[0])
+        tengchongdata.features.forEach(element => {
+            agency = [];
+            agency.push(element.properties.childNum);
+            agency.push(element.properties.name);
+            data_set_from_json.push(agency)
+                // townname.push(element.properties.name);
+                // childNum.push(element.properties.childNum);
+        });
+        // console.log(data_set_from_json)
+
+
+        // console.log(townname)
+        // console.log(childNum)
+        // console.log(data_set_from_json)
+
+        // var data_set = [
+        //     ['amount', 'townname'],
+        //     [1, 'QuShi'],
+        //     [3, 'HeShun'],
+        //     [5, 'WuHe'],
+        //     [10, 'XinHua'],
+        //     [100, 'JieTou'],
+        //     [144, 'MaZhan'],
+        //     [233, 'HeHua'],
+        //     [445, 'MangBang'],
+        //     [666, 'TuanTian'],
+        //     [777, 'QingShui'],
+        //     [3, 'BeiHai'],
+        //     [4, 'PuChan'],
+        //     [56, 'GuDong'],
+        //     [78, 'ZhongHe'],
+        //     [89, 'TengYue'],
+        //     [90, 'DianTan'],
+        //     [23, 'MingGuang'],
+        //     [24, 'HouQiao']
+        // ]
+        // console.log(data_set.length)
+
+        option = {
+            title: {
+                top: '0%',
+                left: 'center',
+                text: 'Rank of Case Number',
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
                 }
             },
-            inverse: true
-        },
-        visualMap: {
-            orient: 'horizontal',
-            left: 'center',
-            min: 10,
-            max: 100,
-            text: ['High Score', 'Low Score'],
-            // Map the score column to color
-            dimension: 0,
-            inRange: {
-                color: ['#65B581', '#FFCE34', '#FD665F']
+            // dataset: {
+            //     source: data_set_from_json
+            // },
+            dataset: {
+                // source: data_set
+                source: data_set_from_json
             },
-            show: false
-        },
-        grid: {
-            left: '11%',
-            top: '8%',
-            right: '10%',
-            bottom: '10%',
-            // containLabel: true
+            grid: { containLabel: true },
+            xAxis: { name: 'amount' },
+            yAxis: {
+                type: 'category',
+                // name: 'Town Name',
+                axisLabel: {
+                    show: true,
+                    textStyle: {
+                        // color: '#fff',
+                        fontSize: '9'
+                    }
+                },
+                inverse: true
+            },
+            // visualMap: {
+            //     orient: 'horizontal',
+            //     left: 'center',
+            //     min: 10,
+            //     max: 100,
+            //     text: ['High Score', 'Low Score'],
+            //     // Map the score column to color
+            //     dimension: 0,
+            //     inRange: {
+            //         color: ['#65B581', '#FFCE34', '#FD665F']
+            //     },
+            //     show: false
+            // },
+            grid: {
+                left: '11%',
+                top: '8%',
+                right: '10%',
+                bottom: '10%',
+                // containLabel: true
 
-        },
-        series: [{
+            },
+            series: [{
 
-            type: 'bar',
-            encode: {
-                // Map the "amount" column to X axis.
-                x: 'amount',
-                // Map the "townname" column to Y axis
-                y: 'townname'
-            }
-        }]
-    };
+                type: 'bar',
+                encode: {
+                    // Map the "amount" column to X axis.
+                    x: 'amount',
+                    // Map the "townname" column to Y axis
+                    y: 'townname'
+                },
+                // 为每个柱子给定颜色，不够的话开始循环
+                itemStyle: {
+                    normal: {
+                        //这里是重点
+                        // 循环这样子方便点 
+                        color: function(params) {
+                            var colorList = ['#FF4933', '#3498DB', '#F4D03F ', '#6C3483 ', '#FF8C33', '#2ECC71', '#2980B9', '#33B7FF', '#334EFF', '#CB33FF', '#943126', '#c23531', '#2f4554', '#61a0a8', '#d48265', '#91c7ae', '#749f83', '#ca8622'];
+                            return colorList[params.dataIndex % colorList.length];
+                        }
+                    }
+                }
+            }]
+        };
 
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
 
 
-    // 把配置项给实例对象，跟着浏览器同比例缩放
-    window.addEventListener('resize', function() {
-        myChart.resize();
-    })
+        // 把配置项给实例对象，跟着浏览器同比例缩放
+        window.addEventListener('resize', function() {
+            myChart.resize();
+        })
+
+    });
+
+    //     });
+    // })(jQuery);
 
 
 })();
